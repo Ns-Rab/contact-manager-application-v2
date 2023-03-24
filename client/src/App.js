124f1017a410d8142ce9b18c9e59e414fc3702db
Ns-Rab <salihuma@gmail.com>
2023-03-24 11:15:16 +0000

    created signup, notfound and home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 
 import './App.css';
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import SignUp from './components/SignUp/SignUp';
+import NotFound from './components/NotFound/NotFound';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
-}
+  return ( <div className="App">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={ <SignUp/> } />
+
+      <Route path="*" element={<NotFound />} />
+
+    
+    </Routes>
+  </div>);
+};
 
 export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Home.css";
+import Footer from './Partials/Footer/Footer';
+import Header from './Partials/Header/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <h1>Home page</h1>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
